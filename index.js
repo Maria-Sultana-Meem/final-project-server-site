6e265675496202bce9182949560633db9774eacf
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-12-05 23:50:32 +0600

    mongodb added

---
+++ index.js
@@ -0,0 +1,158 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const admin = require('firebase-admin')
+const port = process.env.PORT || 3000
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
+  'utf-8'
+)
+const serviceAccount = JSON.parse(decoded)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const app = express()
+// middleware
+app.use(cors()
+)
+app.use(express.json())
+
+// jwt middlewares
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(' ')[1]
+  console.log(token)
+  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  try {
+    const decoded = await admin.auth().verifyIdToken(token)
+    req.tokenEmail = decoded.email
+    console.log(decoded)
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(401).send({ message: 'Unauthorized Access!', err })
+  }
+}
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
+    const db = client.db('loanDB')
+    const loanCollection = db.collection('loans')
+   
+    app.post('/all-loans',async(req,res)=>{
+      const loanData= req.body
+      const result = await loanCollection.insertOne(loanData)
+      res.send(result)
+
+    })
+   
+
+
+
+    // Payment endpoints
+    // app.post('/create-checkout-session', async (req, res) => {
+    //   const paymentInfo = req.body
+    //   console.log(paymentInfo)
+    //   const session = await stripe.checkout.sessions.create({
+    //     line_items: [
+    //       {
+    //         price_data: {
+    //           currency: 'usd',
+    //           product_data: {
+    //             name: paymentInfo?.name,
+    //             description: paymentInfo?.description,
+    //             images: [paymentInfo.image],
+    //           },
+    //           unit_amount: paymentInfo?.price * 100,
+    //         },
+    //         quantity: paymentInfo?.quantity,
+    //       },
+    //     ],
+    //     customer_email: paymentInfo?.customer?.email,
+    //     mode: 'payment',
+    //     metadata: {
+    //       plantId: paymentInfo?.plantId,
+    //       customer: paymentInfo?.customer.email,
+    //     },
+    //     success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+    //     cancel_url: `${process.env.CLIENT_DOMAIN}/plant/${paymentInfo?.plantId}`,
+    //   })
+    //   res.send({ url: session.url })
+    // })
+
+    // app.post('/payment-success', async (req, res) => {
+    //   const { sessionId } = req.body
+    //   const session = await stripe.checkout.sessions.retrieve(sessionId)
+    //   const plant = await plantsCollection.findOne({
+    //     _id: new ObjectId(session.metadata.plantId),
+    //   })
+    //   const order = await ordersCollection.findOne({
+    //     transactionId: session.payment_intent,
+    //   })
+
+    //   if (session.status === 'complete' && plant && !order) {
+    //     // save order data in db
+    //     const orderInfo = {
+    //       plantId: session.metadata.plantId,
+    //       transactionId: session.payment_intent,
+    //       customer: session.metadata.customer,
+    //       status: 'pending',
+    //       seller: plant.seller,
+    //       name: plant.name,
+    //       category: plant.category,
+    //       quantity: 1,
+    //       price: session.amount_total / 100,
+    //       image: plant?.image,
+    //     }
+    //     const result = await ordersCollection.insertOne(orderInfo)
+    //     // update plant quantity
+    //     await plantsCollection.updateOne(
+    //       {
+    //         _id: new ObjectId(session.metadata.plantId),
+    //       },
+    //       { $inc: { quantity: -1 } }
+    //     )
+
+    //     return res.send({
+    //       transactionId: session.payment_intent,
+    //       orderId: result.insertedId,
+    //     })
+    //   }
+    //   res.send(
+    //     res.send({
+    //       transactionId: session.payment_intent,
+    //       orderId: order._id,
+    //     })
+    //   )
+    // })
+
+    // get all orders for a customer by email
+   
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('loanlink server is running..')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
