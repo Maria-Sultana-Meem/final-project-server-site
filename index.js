412dd741fc02e3a87ecb193000f5f6385c65f1b7
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-12-06 19:35:10 +0600

    all loan server

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const admin = require('firebase-admin')
-const port = process.env.PORT || 3000
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
-  'utf-8'
-)
-const serviceAccount = JSON.parse(decoded)
+const admin = require("firebase-admin");
+const port = process.env.PORT || 3000;
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf-8"
+);
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-})
+});
 
-const app = express()
+const app = express();
 // middleware
-app.use(cors()
-)
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 // jwt middlewares
 const verifyJWT = async (req, res, next) => {
-  const token = req?.headers?.authorization?.split(' ')[1]
-  console.log(token)
-  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  const token = req?.headers?.authorization?.split(" ")[1];
+  console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
   try {
-    const decoded = await admin.auth().verifyIdToken(token)
-    req.tokenEmail = decoded.email
-    console.log(decoded)
-    next()
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    console.log(decoded);
+    next();
   } catch (err) {
-    console.log(err)
-    return res.status(401).send({ message: 'Unauthorized Access!', err })
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
   }
-}
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
@@ -42,21 +46,34 @@ const client = new MongoClient(process.env.MONGODB_URI, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 async function run() {
   try {
-    const db = client.db('loanDB')
-    const loanCollection = db.collection('loans')
-   
-    app.post('/all-loans',async(req,res)=>{
-      const loanData= req.body
-      const result = await loanCollection.insertOne(loanData)
-      res.send(result)
+    const db = client.db("loanDB");
+    const loanCollection = db.collection("loans");
 
-    })
-   
+    app.post("/all-loans", async (req, res) => {
+      const loanData = req.body;
+      const result = await loanCollection.insertOne(loanData);
+      res.send(result);
+    });
 
+    app.get("/available-loans", async (req, res) => {
+      const result = await loanCollection.find().limit(6).toArray();
+      res.send(result);
+    });
 
+    app.get("/all-loans", async (req, res) => {
+      const result = await loanCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/all-loans/:id", async (req, res) => {
+       const id = req.params.id;
+
+     const result = await loanCollection.findOne({ _id:new ObjectId(id)});
+      res.send(result);
+    });
 
     // Payment endpoints
     // app.post('/create-checkout-session', async (req, res) => {
@@ -136,23 +153,22 @@ async function run() {
     // })
 
     // get all orders for a customer by email
-   
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('loanlink server is running..')
-})
+app.get("/", (req, res) => {
+  res.send("loanlink server is running..");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
